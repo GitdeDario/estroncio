26fe2fbc7cbdcaf849bc39cc5b0d07e1127cb424
Dario Sosa <dariogss@gmail.com>
2022-05-01 19:21:53 -0300

    Más pruebas interrupciones.

--- estroncioInterrupciones.py
+++ estroncioInterrupciones.py
@@ -109,7 +109,6 @@ def main():
 					)
 		while(ALGUN_BOTON_APRETADO):									#Si alguno de los otros pulsadores (sin ser el del encoder) está presionado, me quedo acá
 			print("algo")
-			time.sleep(2)
 			ALGUN_BOTON_APRETADO = (not(GPIO.input(REPRODUCIR_PAUSA)) 	#Me fijo si alguno de los botones está presionado y si lo está, la variable
 						or not(GPIO.input(ANTERIOR)) 						#ALGUN_BOTON_APRETADO queda en "1". Los "NOT" son porque los botones tiene pull up's
 						or not(GPIO.input(SIGUIENTE)) 						#internos, entonces cuando se presionan, la entrada se pone a tierra ("0"). Así, con
