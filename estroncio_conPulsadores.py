d45ac2b3bdf44c9fa4afa4ba31681a48d8c88e51
Dario Sosa <dariogss@gmail.com>
2022-05-07 17:33:22 -0300

    Prueba 17: pulsadores y LCD

--- estroncio_conPulsadores.py
+++ estroncio_conPulsadores.py
@@ -121,7 +121,7 @@ def main():
 
 			volRegex = re.compile(r'volume:( ){0,2}(\d){1,3}')
 			volumen = volRegex.search(estado_player)
-			v = str(volumen)
+			v = str(volumen.group())
 			vo = v[-3:]
 			print("--------------------")
 			print(vo)
