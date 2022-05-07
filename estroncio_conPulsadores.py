eaede16ac703d7c6a2677f705ca2fa4f75a9daa6
Dario Sosa <dariogss@gmail.com>
2022-05-07 18:26:23 -0300

    Prueba 23: pulsadores y LCD

--- estroncio_conPulsadores.py
+++ estroncio_conPulsadores.py
@@ -140,7 +140,7 @@ def main():
 
 			# Envio el texto al LCD
 			inicio = 0
-			fin = inicio + 14
+			fin = inicio + 15
 			lcd_string(tema[inicio:fin],LCD_LINE_1)
 			lcd_string("vol:"+volumen + "%" + "  " + tiempo, LCD_LINE_2)
 			
