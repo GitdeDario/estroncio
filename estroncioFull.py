0f7fc7f34181f91f910a0c4874020dfce4c46edd
Dario Sosa <dariogss@gmail.com>
2022-05-14 15:20:14 -0300

    Genero el archivo estroncioFull que incorpora el uso del encoder. Los pulsadores deberian seguir funcionando.

--- estroncioFull.py
+++ estroncioFull.py
@@ -65,7 +65,16 @@ CAMBIAR_CROSSFADE = 19
 GPIO.setup(CAMBIAR_CROSSFADE, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 CAMBIAR_RANDOM = 21
-GPIO.setup(CAMBIAR_RANDOM, GPIO.IN, pull_up_down=GPIO.PUD_UP)		
+GPIO.setup(CAMBIAR_RANDOM, GPIO.IN, pull_up_down=GPIO.PUD_UP)	
+
+
+# GPIO usados por el ENCODER
+CLK = 23
+GPIO.setup(CLK, GPIO.IN)	#No necesita pull up interna en la raspi porque el módulo de encoder ya tiene
+DT = 29
+GPIO.setup(DT, GPIO.IN)		#No necesita pull up interna en la raspi porque el módulo de encoder ya tiene
+PULSADOR_ENCODER = 31 
+GPIO.setup(PULSADOR_ENCODER, GPIO.IN) 	#No necesita pull up interna en la raspi porque el módulo de encoder ya tiene
 
 # GPIO usados por el LCD
 LCD_RS = 12
@@ -110,13 +119,14 @@ def main():
 
 	while(True):
 		HAY_ALGO_PARA_EJECUTAR = leer_pulsadores()	#Consulto los pulsadores y veo si hay alguno apretado
+		HAY_ALGO_PARA_EJECUTAR = leer_encoder()
 		espero_a_que_se_libere_el_pulsador()
 
 		if HAY_ALGO_PARA_EJECUTAR:	
-			os.system("mpc"+" "+estado[indice])				#
+			os.system("mpc"+" "+estado[indice])		#
 			HAY_ALGO_PARA_EJECUTAR = False			#
 
-		end = time.time()									#Como acá va a pasar la mayor parte del tiempo, es lógico que esto se imprima acá
+		end = time.time()							#Como acá va a pasar la mayor parte del tiempo, es lógico que esto se imprima acá
 		
 		if (end - start > TIEMPO_REFRESCO_LCD):			#....se imprima o se extraigan estos datos
 			start = time.time()							#
@@ -185,6 +195,59 @@ def leer_pulsadores():
 			indice = 7
 			return True
 
+def leer_encoder():
+	clk_actual = GPIO.input(CLK)
+	dt_actual = GPIO.input(DT)
+	global indice
+	Ei = Er1 = Er2 = Erf = Ei1 = Ei2 = Eif = False	#Estados para la máquina de estados del encoder
+	FINErf = FINEif = True
+	ACTUO_EL_ENCODER = False
+
+	if ((FINErf or FINEif) and (clk_actual == 1) and (dt_actual ==1)):
+		Ei = True
+		Er1 = Er2 = Erf = FINErf = Ei1 = Ei2 = Eif = FINEif = False
+
+	if(Ei and (clk_actual == 0) and (dt_actual ==1)):
+		Er1 = True
+		Ei = Er2 = Erf = FINErf = Ei1 = Ei2 = Eif = FINEif = False
+	if(Er1 and (clk_actual == 0) and (dt_actual ==0)):
+		Er2 = True
+		Ei = Er1 = Erf = FINErf = Ei1 = Ei2 = Eif = FINEif = False
+	if(Er2 and (clk_actual == 1) and (dt_actual ==1)):
+		Erf = True
+		Ei = Er1 = Er2 = FINErf = Ei1 = Ei2 = Eif = FINEif = False
+	if(Erf and (clk_actual == 1) and (dt_actual ==1)):
+		FINErf = True
+		Ei = Er1 = Er2 = Erf = Ei1 = Ei2 = Eif = FINEif = False
+		ACTUO_EL_ENCODER = True
+		if(indice < len(estado)-1):
+			indice += 1
+		else:
+			indice = 0
+		
+		return ACTUO_EL_ENCODER
+
+	if(Ei and (clk_actual == 1) and (dt_actual ==0)):
+		Ei1 = True
+		Ei = Er1 = Er2 = Erf = FINErf = Ei2 = Eif = FINEif = False
+	if(Ei1 and (clk_actual == 0) and (dt_actual ==0)):
+		Ei2 = True
+		Ei = Er1 = Er2 = Erf = FINErf = Ei1 = Eif = FINEif = False
+	if(Ei2 and (clk_actual == 0) and (dt_actual ==1)):
+		Eif = True
+		Ei = Er1 = Er2 = Erf = FINErf = Ei1 = Ei2 = FINEif = False
+	if(Eif and (clk_actual == 1) and (dt_actual ==1)):
+		FINEif = True
+		Ei = Er1 = Er2 = Erf = FINErf = Ei1 = Ei2 = Eif = False
+		ACTUO_EL_ENCODER = True
+		if(indice > 0):
+			indice -= 1
+		else:
+			indice = len(estado)-1
+		
+		return ACTUO_EL_ENCODER
+
+
 def espero_a_que_se_libere_el_pulsador():
 	ALGUN_BOTON_APRETADO = (not(GPIO.input(REPRODUCIR_PAUSA)) 	#Me fijo si alguno de los botones está presionado y si lo está, la variable
 					or not(GPIO.input(ANTERIOR)) 						#ALGUN_BOTON_APRETADO queda en "1". Los "NOT" son porque los botones tiene pull up's
@@ -194,6 +257,7 @@ def espero_a_que_se_libere_el_pulsador():
 					or not(GPIO.input(BAJAR_VOLUMEN)) 					#
 					or not(GPIO.input(CAMBIAR_CROSSFADE))				#
 					or not(GPIO.input(CAMBIAR_RANDOM)) 					#
+					or not(GPIO.input(PULSADOR_ENCODER))
 					)
 	while(ALGUN_BOTON_APRETADO):
 			ALGUN_BOTON_APRETADO = (not(GPIO.input(REPRODUCIR_PAUSA)) 	#Me fijo si alguno de los botones está presionado y si lo está, la variable
@@ -204,6 +268,7 @@ def espero_a_que_se_libere_el_pulsador():
 					or not(GPIO.input(BAJAR_VOLUMEN)) 					#
 					or not(GPIO.input(CAMBIAR_CROSSFADE))				#
 					or not(GPIO.input(CAMBIAR_RANDOM)) 					#
+					or not(GPIO.input(PULSADOR_ENCODER))
 					)
 
 
@@ -246,41 +311,41 @@ def lcd_byte(bits, mode):
   # mode = True  for character
   #        False for command
 
-  GPIO.output(LCD_RS, mode) # RS
+	GPIO.output(LCD_RS, mode) # RS
 
   # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
+	GPIO.output(LCD_D4, False)
+	GPIO.output(LCD_D5, False)
+	GPIO.output(LCD_D6, False)
+	GPIO.output(LCD_D7, False)
+	if bits&0x10==0x10:
+		GPIO.output(LCD_D4, True)
+	if bits&0x20==0x20:
+		GPIO.output(LCD_D5, True)
+	if bits&0x40==0x40:
+		GPIO.output(LCD_D6, True)
+	if bits&0x80==0x80:
+		GPIO.output(LCD_D7, True)
 
   # Toggle 'Enable' pin
-  lcd_toggle_enable()
+	lcd_toggle_enable()
 
   # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
+	GPIO.output(LCD_D4, False)
+	GPIO.output(LCD_D5, False)
+	GPIO.output(LCD_D6, False)
+	GPIO.output(LCD_D7, False)
+	if bits&0x01==0x01:
+		GPIO.output(LCD_D4, True)
+	if bits&0x02==0x02:
+		GPIO.output(LCD_D5, True)
+	if bits&0x04==0x04:
+		GPIO.output(LCD_D6, True)
+	if bits&0x08==0x08:
+		GPIO.output(LCD_D7, True)
 
   # Toggle 'Enable' pin
-  lcd_toggle_enable()
+	lcd_toggle_enable()
 
 def lcd_toggle_enable():
   # Toggle enable
@@ -292,10 +357,10 @@ def lcd_toggle_enable():
 
 def lcd_string(message,line):
   # Send string to display
-  message = message.ljust(LCD_WIDTH," ")
-  lcd_byte(line, LCD_CMD)
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
+	message = message.ljust(LCD_WIDTH," ")
+	lcd_byte(line, LCD_CMD)
+	for i in range(LCD_WIDTH):
+		lcd_byte(ord(message[i]),LCD_CHR)
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
