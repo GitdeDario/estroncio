d476561ccdd0521fb3442e112b09e8a62a46e02e
Dario Sosa <dariogss@gmail.com>
2022-06-19 11:05:14 -0300

    pruebas con leds rgb y pwm

--- pruebaleds.py
+++ pruebaleds.py
@@ -3,23 +3,30 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
-GPIO.setup(19, GPIO.OUT)
-rojo = GPIO.PWM(19, 100)
-rojo.start(100) 
-rojo.stop()
-print("rojo a 100")  
-time.sleep(5) 
-GPIO.output(19,False)
-
-print("rojo a false")
-time.sleep(5)
-GPIO.output(19,True)
-print("rojo a true")
-time.sleep(5)
+RED = 19
+GREEN = 35
+BLUE = 37
+GPIO.setup(RED, GPIO.OUT)
+rojo = GPIO.PWM(RED, 100)
+GPIO.setup(GREEN, GPIO.OUT)
+verde = GPIO.PWM(GREEN, 100)
+GPIO.setup(BLUE, GPIO.OUT)
+azul = GPIO.PWM(BLUE, 100)
+time.sleep(10)
+print("esperando...")
+rojo.start(100)   
+verde.start(100) 
+azul.start(100)  
 
 while True:
     for i in range(100,-1,-1):
         rojo.ChangeDutyCycle(100 - i)
+        time.sleep(0.02)   
+    for i in range(100,-1,-1):
+        verde.ChangeDutyCycle(100 - i)
+        time.sleep(0.02)   
+    for i in range(100,-1,-1):
+        azul.ChangeDutyCycle(100 - i)
         time.sleep(0.02)           
 
     print("Ciclo completo")
