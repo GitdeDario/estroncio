2596938fcea101625edef95c67d7e35898145616
Dario Sosa <dariogss@gmail.com>
2022-06-19 12:19:18 -0300

    pruebas con leds rgb y pwm

--- pruebaleds.py
+++ pruebaleds.py
@@ -14,9 +14,9 @@ verde = GPIO.PWM(GREEN, 100)
 GPIO.setup(BLUE, GPIO.OUT)
 azul = GPIO.PWM(BLUE, 100)
 
-rojo.start(100)   
-verde.start(100) 
-azul.start(100) 
+rojo.start(0)   
+verde.start(0) 
+azul.start(0) 
 print("esperando...")
 time.sleep(10)
  
