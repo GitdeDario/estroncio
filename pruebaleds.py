53d602d889100b388da2465b913f3e08ad9971f4
Dario Sosa <dariogss@gmail.com>
2022-06-19 11:39:37 -0300

    pruebas con leds rgb y pwm

--- pruebaleds.py
+++ pruebaleds.py
@@ -24,8 +24,9 @@ time.sleep(3)
  
 
 while True:
-    rojo.ChangeDutyCycle(80)
-    verde.ChangeDutyCycle(80)
+    rojo.ChangeDutyCycle(0)
+    verde.ChangeDutyCycle(0)
+    azul.ChangeDutyCycle(0)
     # for i in range(100,-1,-1):
     #     rojo.ChangeDutyCycle(100 - i)
     #     time.sleep(0.02)  
