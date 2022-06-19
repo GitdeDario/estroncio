a6d6641f224960d4cf6068c6f5434b360c0bc368
Dario Sosa <dariogss@gmail.com>
2022-06-19 10:44:45 -0300

    pruebas con leds rgb y pwm

--- pruebaleds.py
+++ pruebaleds.py
@@ -1,27 +1,15 @@
-from leds_RGB import *
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-def main():
-    try:
-        GPIO.setmode(GPIO.BOARD)
-        for i in pins:
-            GPIO.setup(pins[i], GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(24, GPIO.OUT)
+rojo = GPIO.PWM(19, 100)
+rojo.start(100)    
 
-        led = Led(pins['Red'], pins['Green'], pins['Blue'])
+while True:
+    for i in range(100,-1,-1):
+        rojo.ChangeDutyCycle(100 - i)
+        time.sleep(0.02)           
 
-        while True:
-            for nombre, color in COLORS.items():
-                print('Color: {0}'.format(nombre))
-                led.set_color(color)
-                time.sleep(2)
-                reset()
-                # led.set_color(0x000000)
-        led.stop()
-        GPIO.output(pins, GPIO.HIGH)
-    except Exception as e:
-        print(e)
-    finally:
-        GPIO.cleanup()
-
-
-if __name__ == '__main__':
-    main()
+    print("Ciclo completo")
