79fcd8ab78fcfe3ec41f79766cf4c52f55453da3
Dario Sosa <dariogss@gmail.com>
2022-06-26 13:05:38 -0300

    creo archivo para probar servo

--- testSERVO.py
+++ testSERVO.py
@@ -9,7 +9,7 @@ p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) 
 try:
   while True:
-    p.ChangeDutyCycle(5)
+    p.ChangeDutyCycle(1)
     time.sleep(1)
     # p.ChangeDutyCycle(7.5)
     # time.sleep(1)
@@ -17,8 +17,8 @@ try:
     # time.sleep(1)
     # p.ChangeDutyCycle(12.5)
     # time.sleep(1)
-    p.ChangeDutyCycle(10)
-    time.sleep(1)
+    # p.ChangeDutyCycle(10)
+    # time.sleep(1)
     # p.ChangeDutyCycle(7.5)
     # time.sleep(1)
     # p.ChangeDutyCycle(5)
