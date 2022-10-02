bca5541c835de57830e98714333e0f685f137d96
GitdeDario <dariogss@gmail.com>
2022-10-02 09:47:43 -0300

    Modifico para que arranque en stop

--- estroncioFull.py
+++ estroncioFull.py
@@ -130,7 +130,7 @@ GPIO.output(AZUL, True)		# Arrancamos con el led apagado. True lo apaga porque l
 #SI SE AGREGAN FUNCIONES, PONERLAS EN EL FINAL DE ESTA LISTA PARA ASÍ NO AFECTAR EL FUNCIONAMIENTO QUE SE TIENE HASTA EL MOMENTO.
 estado = ["play", "prev", "next", "stop", "volume +10", "volume -10", "crossfade", "random", "pause", "off"]
 indice_temp = 0
-indice = 0
+indice = 3	# Arranca en stop
 FLAG_primera_entrada = True
 
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -143,10 +143,11 @@ def main():
 	lcd_init()
 	start = time.time()
 	song = random.randint(1, largoListaCanciones)
-	os.system("mpc play" +" "+ str(song)) ###################BORRAR ESTO!!!!!!!!!!!!!!!!!!!!!!!!!
+	#os.system("mpc play" +" "+ str(song)) ###################BORRAR ESTO!!!!!!!!!!!!!!!!!!!!!!!!!
+	os.system("mpc stop")	# Arrancamos en stop
 	desde = 0	# para mostrar cadena de texto en el LCD
 	global indice
-	
+
 	while(True):
 		
 		if (estado[indice] == "stop"):
