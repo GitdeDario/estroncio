0f7c26f3051b954365616995ef072d760f5a363b
GitdeDario <dariogss@gmail.com>
2022-10-02 09:29:15 -0300

    Borro un import que se generó automático

--- estroncioFull.py
+++ estroncioFull.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os, random, time, re
-from tkinter import CENTER
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 #---------------------------------------------------------------------------------------------------------------
