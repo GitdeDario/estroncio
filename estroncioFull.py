7921f36bbdfbf0236fade7ee50e744b3637d3b92
GitdeDario <dariogss@gmail.com>
2022-10-02 10:14:05 -0300

    dd

--- estroncioFull.py
+++ estroncioFull.py
@@ -379,10 +379,8 @@ def esperar_enter_encoder():
 			apagar()		
 
 		lcd_string("OK".center(LCD_WIDTH), LCD_LINE_1)						#
-		if estado[indice] == "random":
-			lcd_string(estado[indice_temp].upper().center(LCD_WIDTH) + info_reproduciendo()[4], LCD_LINE_2)
-		else:
-			lcd_string(estado[indice_temp].upper().center(LCD_WIDTH), LCD_LINE_2)								#
+		print(info_reproduciendo()[4])
+		lcd_string(estado[indice_temp].upper().center(LCD_WIDTH), LCD_LINE_2)								#
 		indice = indice_temp
 		time.sleep(1)
 	
