f3b65943a5e1c47020e7d6526443594231ac7d17
GitdeDario <dariogss@gmail.com>
2022-10-02 10:18:24 -0300

    sigo probando

--- estroncioFull.py
+++ estroncioFull.py
@@ -199,7 +199,7 @@ def main():
 			end = time.time()							# Como acá va a pasar la mayor parte del tiempo, es lógico que esto se imprima acá
 			if (end - start > TIEMPO_REFRESCO_LCD):		# ....se imprima o se extraigan estos datos
 				start = time.time()						#
-				(volumen, tema, tiempo, tiempo_total, estado_random) = info_reproduciendo()
+				(volumen, tema, tiempo, tiempo_total) = info_reproduciendo()
 				lcd_string(tema[desde:]+"  *  "+tema[:desde], LCD_LINE_1)		# Envio el texto al LCD de forma tal que se muestra
 				if (desde < len(tema) and tiempo_total != "100%"):				# circularmente el tema
 					desde += 1													#
@@ -379,8 +379,8 @@ def esperar_enter_encoder():
 			apagar()		
 
 		lcd_string("OK".center(LCD_WIDTH), LCD_LINE_1)						#
-		print(info_reproduciendo()[4])
-		lcd_string(estado[indice_temp].upper().center(LCD_WIDTH), LCD_LINE_2)								#
+		lcd_string(estado[indice_temp].upper().center(LCD_WIDTH), LCD_LINE_2)		
+		print(info_reproduciendo()[4])						#
 		indice = indice_temp
 		time.sleep(1)
 	
@@ -409,10 +409,10 @@ def info_reproduciendo():
 	tiempo_totalRaw = tiempo_totalRegex.search(estado_player)	# cuando se alcanza el 100% y que lo que se muesra arranque desde
 	tiempo_total = str(tiempo_totalRaw.group())[-3:]			# el principio: INTERPRETE - TEMA	
 
-	randomRegex = re.compile(r'random:( ){0,2}(\d){1,3}')		#Idem con el estado de la funcion random
-	randomRaw = randomRegex.search(estado_player)				#
-	estado_random = str(randomRaw.group())[5:]					#Elimino el "Flas/" del inicio
-	
+	#randomRegex = re.compile(r'random:( ){0,2}(\d){1,3}')		#Idem con el estado de la funcion random
+	#randomRaw = randomRegex.search(estado_player)				#
+	#estado_random = str(randomRaw.group())[5:]					#Elimino el "Flas/" del inicio
+	estado_random = "BLA"	
 
 	return (volumen, tema, tiempo, tiempo_total, estado_random)
 
