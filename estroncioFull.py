5d9be304e52776653a1862bc706c0b00cc9f6084
GitdeDario <dariogss@gmail.com>
2022-10-02 21:14:50 -0300

    fds

--- estroncioFull.py
+++ estroncioFull.py
@@ -493,10 +493,8 @@ def accionar_tapa(pasos):
 			# Acá esto nunca pasaría porque siempre debería actuar antes los botones de fin de carrera
 			if (StepCounter == StepCount):
 				StepCounter = 0
-				return
 			if (StepCounter < 0):
 				StepCounter = StepCount-1
-				return
 			# Wait before moving on
 			time.sleep(0.005)
 	
