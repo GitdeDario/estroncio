1d023fcb3459d219c3802b2a67127756a674493d
GitdeDario <dariogss@gmail.com>
2022-10-06 19:27:01 -0300

    dfd

--- estroncioFull.py
+++ estroncioFull.py
@@ -482,7 +482,7 @@ def accionar_tapa(pasos):
 	
 	while not fin_de_carrera:
 		for i in range(pasos):
-			fin_de_carrera = GPIO.input(TOPE_PUERTA_ABIERTA) or GPIO.input(TOPE_PUERTA_CERRADA)
+			fin_de_carrera = input("edfj")#GPIO.input(TOPE_PUERTA_ABIERTA) or GPIO.input(TOPE_PUERTA_CERRADA)
 			print(fin_de_carrera)
 			for pin in range(4):
 				xpin = StepperPins[pin]
