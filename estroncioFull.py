c614863a798a07c90def778a92e7b87a7ae10a30
GitdeDario <dariogss@gmail.com>
2022-10-07 20:08:48 -0300

    sadfdsf

--- estroncioFull.py
+++ estroncioFull.py
@@ -152,7 +152,7 @@ GPIO.output(AZUL, True)		# Arrancamos con el led apagado. True lo apaga porque l
 #SI SE AGREGAN FUNCIONES, PONERLAS EN EL FINAL DE ESTA LISTA PARA ASÍ NO AFECTAR EL FUNCIONAMIENTO QUE SE TIENE HASTA EL MOMENTO.
 estado = ["play", "prev", "next", "stop", "volume +10", "volume -10", "random", "pause", "off"]
 indice_temp = 0
-indice = 0	# 
+indice = 3	# 
 FLAG_primera_entrada = True
 
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -167,9 +167,8 @@ def main():
 	lcd_init()
 	start = time.time()
 	song = random.randint(1, largoListaCanciones)
-	os.system("mpc play" +" "+ str(song)) ###################BORRAR ESTO!!!!!!!!!!!!!!!!!!!!!!!!!
-	#os.system("mpc stop")	# Arrancamos en stop
-	#indice = 3
+	#os.system("mpc play" +" "+ str(song)) ###################BORRAR ESTO!!!!!!!!!!!!!!!!!!!!!!!!!
+	os.system("mpc stop")	# Arrancamos en stop
 	desde = 0	# para mostrar cadena de texto en el LCD
 
 	while(True):
